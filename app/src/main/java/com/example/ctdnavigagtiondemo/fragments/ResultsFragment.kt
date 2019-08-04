e504d11b318d2edd4b535ae67b759ccbd3c9f268
Aubrey Landau <aubreylandau@gmail.com>
2019-08-04 15:27:54 -0500

    finished assignment

--- app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
+++ app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.ctdnavigagtiondemo.R
+import com.example.ctdnavigagtiondemo.models.RPS
 import kotlinx.android.synthetic.main.fragment_results.*
+import kotlin.random.Random
 
 
 class ResultsFragment : Fragment() {
@@ -25,8 +28,32 @@ class ResultsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        buttonPlayAgain.setOnClickListener {
+        val userChoice: RPS = arguments?.get("user_choice") as RPS
+        textViewPlayerChoice.text = userChoice.name
+        val playerNum = when (userChoice.name) {
+            "ROCK" -> 0
+            "PAPER" -> 1
+            else -> 2
+        }
+        val computer = Random.nextInt(0, 2)
+        textViewAiChoice.text = when(computer) {
+            0 -> "ROCK"
+            1 -> "PAPER"
+            else -> "SCISSORS"
+        }
 
+        textViewResult.text = when ((computer + 3 - playerNum) % 3) {
+            0 -> "You tied."
+            1 -> "You lost..."
+            2 -> "You won!"
+            else -> "how did you get here?"
+        }
+
+        buttonPlayAgain.setOnClickListener {
+            findNavController().navigate(R.id.action_global_startFragment)
+        }
+        buttonCredits.setOnClickListener{
+            findNavController().navigate(R.id.action_resultsFragment_to_creditsFragment)
         }
     }
 }
