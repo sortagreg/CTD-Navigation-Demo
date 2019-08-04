5c521a129f8fca9e5611bcb11337f1865a47107e
yunqili0809 <42676224+yunqili0809@users.noreply.github.com>
2019-08-04 15:20:53 -0500

    Revert "np"
    This reverts commit cd92b8906fd1eaa710d735637af763755d7e9dae.

--- app - Copy/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
+++ app - Copy/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
@@ -10,6 +10,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.ctdnavigagtiondemo.R
 import com.example.ctdnavigagtiondemo.models.RPS
 import kotlinx.android.synthetic.main.fragment_results.*
+import kotlinx.android.synthetic.main.fragment_start.*
 import kotlin.random.Random
 
 
@@ -26,32 +27,10 @@ class ResultsFragment : Fragment() {
             R.layout.fragment_results, container, false
         )
 
-
-     fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val userChoise: RPS = arguments?.get(getString(R.string.USER_CHOISE)) as RPS
-        val aIInput :RPS
-        var aIChoise = Random.nextInt(1, 3)
-        when(aIChoise){
-
-            1-> aIInput = RPS.ROCK
-            2-> aIInput = RPS.PAPER
-            3-> aIInput = RPS.SCISSORS
-            else->"ERROR - INVALID ENTRY"
-        }
-        var aI : RPS = arguments?.get(getString(aIChoise)) as RPS
-        textViewPlayerChoice.text = userChoise.name
-        textViewAiChoice.text= aI.name
-        when(userChoise){
-            RPS.ROCK-> if (aI.equals(RPS.PAPER)) textViewResult.text = "You won!!!" else textViewResult.text = "You loose!!!"
-            RPS.PAPER-> if (aI.equals(RPS.SCISSORS))textViewResult.text = "You won!!!" else textViewResult.text = "You loose!!!"
-            RPS.SCISSORS-> if (aI.equals(RPS.ROCK))textViewResult.text = "You won!!!" else textViewResult.text = "You loose!!!"
-        }
-        buttonPlayAgain.setOnClickListener {
-            findNavController().navigate(R.id.action_global_startFragment)
-
+}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        buttonPlayGame.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_playGameFragment)
         }
     }
-
-
-}
 }
