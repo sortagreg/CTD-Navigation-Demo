fbcecf5485e37cc84962bb9d3378994f72bc9653
OwenHHHH <owenhandelman@gmail.com>
2019-08-05 07:27:52 -0500

    finnished app with painting
    painting has an extra three squares and it gets messed up when viewd on my emulator

--- app/src/main/java/com/example/ctdnavigagtiondemo/MainActivity.kt
+++ app/src/main/java/com/example/ctdnavigagtiondemo/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.ctdnavigagtiondemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean{
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
--- app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
+++ app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.ctdnavigagtiondemo.R
+import com.example.ctdnavigagtiondemo.models.RPS
 import kotlinx.android.synthetic.main.fragment_results.*
+import kotlinx.android.synthetic.main.fragment_start.*
+import kotlin.random.Random
 
 
 class ResultsFragment : Fragment() {
@@ -19,14 +23,36 @@ class ResultsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
+
+
         return inflater.inflate(
             R.layout.fragment_results, container, false
         )
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        buttonPlayAgain.setOnClickListener {
+        val userChoice: RPS = arguments?.get(getString(R.string.USERCHOICE)) as RPS
 
+        textViewPlayerChoice.text = userChoice.name
+
+        var aiChoice = RPS.PAPER
+        val aiInput = Random.nextInt(1, 3)
+        if (aiInput == 1) aiChoice = RPS.PAPER else if (aiInput == 2) aiChoice = RPS.ROCK else aiChoice = RPS.SCISSORS
+
+        textViewAiChoice.text = aiChoice.name
+
+
+        when {
+            userChoice == RPS.PAPER -> if (aiChoice == RPS.SCISSORS) textViewResult.text = "You Lost!" else if (aiChoice == userChoice) {textViewResult.text = "You Tied!"} else textViewResult.text = "You Won!"
+            userChoice == RPS.SCISSORS -> if (aiChoice == RPS.ROCK) textViewResult.text = "You Lost!" else if (aiChoice == userChoice) {textViewResult.text = "You Tied!"} else textViewResult.text = "You Won!"
+            userChoice == RPS.ROCK -> if (aiChoice == RPS.PAPER) textViewResult.text = "You Lost!" else if (aiChoice == userChoice) {textViewResult.text = "You Tied!"} else textViewResult.text = "You Won!"
+        }
+        buttonPlayAgain.setOnClickListener {
+            findNavController().navigate(R.id.action_global_startFragment)
+        }
+        picturebutton.setOnClickListener{
+            findNavController().navigate(R.id.action_resultsFragment_to_creditsFragment)
         }
     }
 }
