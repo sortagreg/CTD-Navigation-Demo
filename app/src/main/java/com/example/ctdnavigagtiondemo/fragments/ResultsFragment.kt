3a457085aff8afb5c56098f9a5886d9c30b40efd
Hekireki21 <53225522+Hekireki21@users.noreply.github.com>
2019-08-05 16:13:08 -0500

    1

--- app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
+++ app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.ctdnavigagtiondemo.R
+import com.example.ctdnavigagtiondemo.models.RPS
 import kotlinx.android.synthetic.main.fragment_results.*
 
 
@@ -25,8 +27,33 @@ class ResultsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        buttonPlayAgain.setOnClickListener {
+        val userChoice: RPS = arguments?.get("user_choice") as RPS
+        val aiChoice: RPS = arguments?.get("ai_choice") as RPS
 
+
+        textViewPlayerChoice.text = userChoice.name
+        textViewAiChoice.text = aiChoice.name
+
+        buttonPlayAgain.setOnClickListener {
+            findNavController().navigate(R.id.action_global_startFragment)
         }
+
+
+        private fun findResult(a: RPS, aI: RPS): String = when (a) {
+            aI -> getString(R.string.draw)
+            RPS.ROCK -> if (aI == RPS.PAPER) getString(R.string.lost) else getString(R.string.won)
+            RPS.PAPER -> if (aI == RPS.SCISSORS) getString(R.string.lost) else getString(R.string.won)
+            RPS.SCISSORS -> if (aI == RPS.ROCK) getString(R.string.lost) else getString(R.string.won)
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
