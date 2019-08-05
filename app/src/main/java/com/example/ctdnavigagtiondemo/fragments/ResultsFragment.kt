41160bcd16e77a8cdfba210dbc64ca2bfebb763c
Lucas <lucas@LucasdeMacBook-Pro.local>
2019-08-05 07:40:45 -0500

    2

--- app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
+++ app/src/main/java/com/example/ctdnavigagtiondemo/fragments/ResultsFragment.kt
@@ -9,11 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.ctdnavigagtiondemo.R
 import com.example.ctdnavigagtiondemo.models.RPS
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_results.*
-import java.sql.PreparedStatement
-import kotlin.math.nextDown
-import kotlin.random.Random
 
 
 class ResultsFragment : Fragment() {
@@ -25,25 +21,39 @@ class ResultsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
+
+
         return inflater.inflate(
             R.layout.fragment_results, container, false
         )
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val userChoice: RPS = arguments?.get("user_choice") as RPS
+        val aiChoice = RPS.values().random()
+        val result = getResult(userChoice, aiChoice)
 
-        val userChoice: RPS = arguments?.get(getString(R.string.USER_CHOICE)) as RPS
 
         textViewPlayerChoice.text = userChoice.name
+        textViewAiChoice.text = aiChoice.name
+        textViewResult.text = result
 
         buttonPlayAgain.setOnClickListener {
             findNavController().navigate(R.id.action_global_startFragment)
+
         }
 
         buttonCredit.setOnClickListener {
-                findNavController().navigate(R.id.action_resultsFragment_to_creditsFragments)
+            findNavController().navigate(R.id.action_resultsFragment_to_creditsFragments)
         }
 
+    }
+
+    private fun getResult(person1: RPS, person2:RPS): String = when (person1){
+        person2 -> getString(R.string.you_tied)
+
+        RPS.ROCK -> if (person2 == RPS.PAPER) getString(R.string.you_lost) else getString(R.string.you_won)
+        RPS.PAPER -> if (person2 == RPS.SCISSORS) getString(R.string.you_lost) else getString(R.string.you_won)
+        RPS.SCISSORS -> if (person2 == RPS.ROCK) getString(R.string.you_lost) else getString(R.string.you_won)
     }
 }
