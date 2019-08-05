5e57c419d2207969a54fd163118ce5d86bb30d10
OwenHHHH <owenhandelman@gmail.com>
2019-08-05 08:11:21 -0500

    Update CreditsFragment.kt

--- app/src/main/java/com/example/ctdnavigagtiondemo/fragments/CreditsFragment.kt
+++ app/src/main/java/com/example/ctdnavigagtiondemo/fragments/CreditsFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.example.ctdnavigagtiondemo.R
+import kotlinx.android.synthetic.main.fragment_credits.*
+import kotlinx.android.synthetic.main.fragment_results.*
 
 
 /**
@@ -22,6 +25,8 @@ class CreditsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_credits, container, false)
+
+
 
 
     }
